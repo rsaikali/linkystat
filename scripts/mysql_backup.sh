d466a12978ff84f29f6da50d156c9d9bf427c7f5
Roland SAIKALI <roland@saikali.fr>
2023-11-22 10:35:30 +0100

    Don't need email address anymore

--- scripts/mysql_backup.sh
+++ scripts/mysql_backup.sh
@@ -5,9 +5,5 @@ mysql_docker_id=$(docker ps -f name=mysql --format "{{.ID}}")
 mysql_root_password=$(docker exec ${mysql_docker_id} printenv MYSQL_ROOT_PASSWORD)
 mysql_database=$(docker exec ${mysql_docker_id} printenv MYSQL_DATABASE)
 
-# Get Grafana administrator email address
-grafana_docker_id=$(docker ps -f name=grafana --format "{{.ID}}")
-grafana_admin_email=$(docker exec ${grafana_docker_id} printenv GF_SECURITY_ADMIN_EMAIL)
-
 # MySQL dump database to timestamped backup file
 docker exec ${mysql_docker_id} /usr/bin/mysqldump -u root --password=${mysql_root_password} --insert-ignore --skip-triggers --compact --no-create-db --no-create-info -B ${mysql_database} | gzip -c > ./linkystat_mysql_backup.sql.gz
