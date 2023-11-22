1c25c5af9188cae0b1397e875763d8555ca026d5
Roland SAIKALI <roland@saikali.fr>
2023-11-22 10:41:23 +0100

    Better deploy steps (build/stop/start)

--- scripts/deploy_dev.sh
+++ scripts/deploy_dev.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
 
+docker compose --env-file ../env/.env.dev -f ../docker-compose.yaml -f ../docker-compose.dev.yaml build --pull
 docker compose --env-file ../env/.env.dev stop --timeout 1 
-docker compose --env-file ../env/.env.dev -f ../docker-compose.yaml -f ../docker-compose.dev.yaml up --build -d
+docker compose --env-file ../env/.env.dev -f ../docker-compose.yaml -f ../docker-compose.dev.yaml up -d
