f3a5a7cd06b031340497e39a583d984902391434
Roland SAIKALI <roland@saikali.fr>
2023-11-22 09:47:02 +0100

    Simpler MySQL backup to prepare for GoogleDrive artifact backup

--- scripts/mysql_backup.sh
+++ scripts/mysql_backup.sh
@@ -1,13 +1,8 @@
 #!/bin/bash -e
 
-backup_directory=~/linkystat_backups
-
-# Create backup directory
-mkdir -p ${backup_directory}
-
 # Define backup filename (timestamped)
 current_date=$(date '+%Y-%m-%d')
-backup_file="${backup_directory}/mysql_backup_linkystat_${current_date}.sql.gz"
+backup_file="./mysql_backup_linkystat_${current_date}.sql.gz"
 
 # Get MySQL password and database
 mysql_docker_id=$(docker ps -f name=mysql --format "{{.ID}}")
@@ -21,10 +16,4 @@ grafana_admin_email=$(docker exec ${grafana_docker_id} printenv GF_SECURITY_ADMI
 # MySQL dump database to timestamped backup file
 docker exec ${mysql_docker_id} /usr/bin/mysqldump -u root --password=${mysql_root_password} --insert-ignore --skip-triggers --compact --no-create-db --no-create-info -B ${mysql_database} | gzip -c > ${backup_file}
 
-# Send mail with timestamped backup file attached
-backup_size=$(du -sk ${backup_file} | cut -f1)
-echo "LinkyStat MySQL backup size on ${current_date} is ${backup_size}ko" | mail -s "LinkyStat MySQL backup ${current_date} [${backup_size}ko]" ${grafana_admin_email} -A ${backup_file}
-
-# Keep only 3 latest backup files
-cd ${backup_directory}
-rm `ls -t ${backup_directory} | awk 'NR>3'` || true
+cp ${backup_file} "./mysql_backup_linkystat.sql.gz"
