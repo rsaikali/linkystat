8b3c77510ff6a92cbfe16bf6fb5b811ce92bb603
Roland Saïkali <roland@saikali.fr>
2024-07-02 14:53:19 +0200

    No more needed

--- scripts/deploy_dev.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-
-docker compose --env-file ../env/.env.dev -f ../docker-compose.yaml -f ../docker-compose.dev.yaml build --pull
-docker compose --env-file ../env/.env.dev stop --timeout 1 
-docker compose --env-file ../env/.env.dev -f ../docker-compose.yaml -f ../docker-compose.dev.yaml up -d
