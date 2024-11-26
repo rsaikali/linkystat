dc320aa78ac03c31e95f197e0f84be79681436d4
Roland Saïkali <roland@saikali.fr>
2024-11-26 09:56:55 +0100

    Realtime history 48 hours retention

--- files/mysql/init-script.sh
+++ files/mysql/init-script.sh
@@ -126,7 +126,7 @@ delimiter ;;
 CREATE EVENT IF NOT EXISTS clean_realtime ON SCHEDULE EVERY 1 MINUTE 
 DO
 BEGIN
-	DELETE FROM linky_realtime WHERE time < NOW() - INTERVAL 7 DAY;
+	DELETE FROM linky_realtime WHERE time < NOW() - INTERVAL 2 DAY;
 END;;
 delimiter ;
 "
