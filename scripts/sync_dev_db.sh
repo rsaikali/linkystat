8403fd35590029b5dffc0c2fe6ab8594118d2708
Roland Saïkali <roland@saikali.fr>
2025-09-11 18:06:45 +0200

    Better logging info

--- scripts/sync_dev_db.sh
+++ scripts/sync_dev_db.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -e
 
+echo "Starting database sync from production to local..."
+
 # Backup production MySQL database
+echo "Backing up production database..."
 ssh $PRODUCTION_USER@$PRODUCTION_DB_HOST /bin/bash <<EOF
     mysql_docker_id=\$(docker ps -f name=mysql --format "{{.ID}}")
     mysql_root_password=\$(docker exec \${mysql_docker_id} printenv MYSQL_ROOT_PASSWORD)
@@ -9,11 +12,16 @@ ssh $PRODUCTION_USER@$PRODUCTION_DB_HOST /bin/bash <<EOF
 EOF
 
 # Copy new SQL dump file locally
+echo "Copying backup file locally..."
 scp $PRODUCTION_USER@$PRODUCTION_DB_HOST:/tmp/backup.sql.gz .
 
 # Restore local MySQL database
 mysql_docker_id=$(docker ps -f name=mysql --format "{{.ID}}")
+echo "Restoring database in docker container $mysql_docker_id"
 zcat backup.sql.gz | docker exec -i ${mysql_docker_id} /usr/bin/mysql -u root --password=$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE
 
 # Remove SQL dump file
+echo "Cleaning up..."
 rm -Rf backup.sql.gz
+
+
